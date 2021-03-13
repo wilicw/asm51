487ec3ad90c139a58786f6c9ea5db7ae8b577904
William Chang <wilicw1346@gmail.com>
2021-03-13 10:19:29 +0800

    Add SFRs bit map

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ label_dict = {}
 
 SFRs = {
     "A": 0xE0,
+    "ACC": 0xE0,
     "B": 0xF0,
     "PSW": 0xD0,
     "IP": 0xB8,
@@ -30,6 +31,100 @@ SFRs = {
     "P0": 0x80,
 }
 
+SFRs_bit = {
+    "P0.0": 0x80,
+    "P0.1": 0x81,
+    "P0.2": 0x82,
+    "P0.3": 0x83,
+    "P0.4": 0x84,
+    "P0.5": 0x85,
+    "P0.6": 0x86,
+    "P0.7": 0x87,
+    "P1.0": 0x90,
+    "P1.1": 0x91,
+    "P1.2": 0x92,
+    "P1.3": 0x93,
+    "P1.4": 0x94,
+    "P1.5": 0x95,
+    "P1.6": 0x96,
+    "P1.7": 0x97,
+    "P2.0": 0xA0,
+    "P2.1": 0xA1,
+    "P2.2": 0xA2,
+    "P2.3": 0xA3,
+    "P2.4": 0xA4,
+    "P2.5": 0xA5,
+    "P2.6": 0xA6,
+    "P2.7": 0xA7,
+    "P3.0": 0xB0,
+    "P3.1": 0xB1,
+    "P3.2": 0xB2,
+    "P3.3": 0xB3,
+    "P3.4": 0xB4,
+    "P3.5": 0xB5,
+    "P3.6": 0xB6,
+    "P3.7": 0xB7,
+    "TCON.0": 0x88,
+    "TCON.1": 0x89,
+    "TCON.2": 0x8A,
+    "TCON.3": 0x8B,
+    "TCON.4": 0x8C,
+    "TCON.5": 0x8D,
+    "TCON.6": 0x8E,
+    "TCON.7": 0x8F,
+    "SCON.0": 0x98,
+    "SCON.1": 0x99,
+    "SCON.2": 0x9A,
+    "SCON.3": 0x9B,
+    "SCON.4": 0x9C,
+    "SCON.5": 0x9D,
+    "SCON.6": 0x9E,
+    "SCON.7": 0x9F,
+    "IE.0": 0xA8,
+    "IE.1": 0xA9,
+    "IE.2": 0xAA,
+    "IE.3": 0xAB,
+    "IE.4": 0xAC,
+    "IE.7": 0xAF,
+    "IP.0": 0xB8,
+    "IP.1": 0xB9,
+    "IP.2": 0xBA,
+    "IP.3": 0xBB,
+    "IP.4": 0xBC,
+    "PSW.0": 0xD0,
+    "PSW.1": 0xD1,
+    "PSW.2": 0xD2,
+    "PSW.3": 0xD3,
+    "PSW.4": 0xD4,
+    "PSW.5": 0xD5,
+    "PSW.6": 0xD6,
+    "PSW.7": 0xD7,
+    "ACC.0": 0xE0,
+    "ACC.1": 0xE1,
+    "ACC.2": 0xE2,
+    "ACC.3": 0xE3,
+    "ACC.4": 0xE4,
+    "ACC.5": 0xE5,
+    "ACC.6": 0xE6,
+    "ACC.7": 0xE7,
+    "A.0": 0xE0,
+    "A.1": 0xE1,
+    "A.2": 0xE2,
+    "A.3": 0xE3,
+    "A.4": 0xE4,
+    "A.5": 0xE5,
+    "A.6": 0xE6,
+    "A.7": 0xE7,
+    "B.0": 0xF0,
+    "B.1": 0xF1,
+    "B.2": 0xF2,
+    "B.3": 0xF3,
+    "B.4": 0xF4,
+    "B.5": 0xF5,
+    "B.6": 0xF6,
+    "B.7": 0xF7,
+}
+
 
 class syntax_match():
     def __init__(self) -> None:
@@ -37,7 +132,8 @@ class syntax_match():
         self.normal_word = lambda x: re.match(r"^(\w*?)$", x)  # LABEL
         self.hex = lambda x: re.match(r"^([0-9A-F]+?)H$", x)  # EEH
         self.dec = lambda x: re.match(r"^([0-9]+?)$", x)  # 255
-        self.imm_hex = lambda x: re.match(r"^#([0-9A-F]+?)H$", x)  # hashtag EEH
+        self.imm_hex = lambda x: re.match(r"^#([0-9A-F]+?)H$", x
+                                          )  # hashtag EEH
         self.imm_dec = lambda x: re.match(r"^#([0-9]+?)$", x)  # hashtag 255
         self.internal_R_ram = lambda x: re.match(r"^@R(\d?)", x)  # @R0
         self.general_reg = lambda x: re.match(r"^R(\d?)", x)  # R7
@@ -337,7 +433,7 @@ def parser(asm_code):
                     write_rom([0x42, int(v[1])])
                 else:
                     ins_err(ins, f_line)
-            elif (v:= sym.imm_hex(args[1]))!=None:
+            elif (v := sym.imm_hex(args[1])) != None:
                 immediate = int(v[1], 16)
                 if (v := sym.sfr(args[0])) != None:
                     write_rom([0x43, sfr_hex(v), immediate])
@@ -347,7 +443,7 @@ def parser(asm_code):
                     write_rom([0x43, int(v[1]), immediate])
                 else:
                     ins_err(ins, f_line)
-            elif (v:= sym.imm_dec(args[1]))!=None:
+            elif (v := sym.imm_dec(args[1])) != None:
                 immediate = int(v[1])
                 if (v := sym.sfr(args[0])) != None:
                     write_rom([0x43, sfr_hex(v), immediate])
@@ -388,7 +484,7 @@ def parser(asm_code):
                     write_rom([0x52, int(v[1])])
                 else:
                     ins_err(ins, f_line)
-            elif (v:= sym.imm_hex(args[1]))!=None:
+            elif (v := sym.imm_hex(args[1])) != None:
                 immediate = int(v[1], 16)
                 if (v := sym.sfr(args[0])) != None:
                     write_rom([0x53, sfr_hex(v), immediate])
@@ -398,7 +494,7 @@ def parser(asm_code):
                     write_rom([0x53, int(v[1]), immediate])
                 else:
                     ins_err(ins, f_line)
-            elif (v:= sym.imm_dec(args[1]))!=None:
+            elif (v := sym.imm_dec(args[1])) != None:
                 immediate = int(v[1])
                 if (v := sym.sfr(args[0])) != None:
                     write_rom([0x53, sfr_hex(v), immediate])
